5fda43fad89b3bd4811efbe52e2a5c6acb43efd2
woodwardtw <twwoodward@me.com>
2022-08-06 08:15:15 -0400

    import option
    auto-load acf fix
    auto deleter for 48k post creation mess
    filter to flip names

--- inc/acf.php
+++ inc/acf.php
@@ -8,35 +8,110 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-	//save acf json
-		add_filter('acf/settings/save_json', 'oerxdomains_json_save_point');
-		 
-		function oerxdomains_json_save_point( $path ) {
-		    
-		    // update path
-		    $path = plugin_dir_path(__FILE__) . '/acf-json'; //replace w get_stylesheet_directory() for theme
-		    
-		    
-		    // return
-		    return $path;
-		    
-		}
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page('Conference Import');
+	
+}
+
+//from https://rubendivine.com/run-php-function-when-updating-wordpress-acf-options-page/
+add_action('acf/save_post', 'alt_oer_csv_import');
+function alt_oer_csv_import(){
+    $screen = get_current_screen();
+    if (strpos($screen->id, "acf-options-conference-import") == true) {
+        // Function code here: 
+        $speakers = get_field('speaker_csv', 'option');
+        $presentations = get_field('presentation_csv', 'option');
+        if($presentations){
+        	alt_oer_parse_sessions_csv($presentations);
+        }
+        if($speakers){
+        	alt_oer_parse_speaker_csv($speakers);
+        }
+    }
+}
+
+function alt_oer_csv_make_sessions($title, $description){
+	$args = array(
+	  'post_title'    => wp_strip_all_tags($title),
+	  'post_type'     => 'presentation',
+	  'post_status'  => 'publish',	 
+	);
+	$post_id = wp_insert_post($args);
+	update_field('session_description', $description, $post_id);
+}
+
+function alt_oer_parse_sessions_csv($file_path){
+	 if(isset($file_path)){
+    
+    	$filename = $file_path;    
+    
+        $file = fopen($filename, "r");
+        fgetcsv($file);
+          while (($getData = fgetcsv($file, 1000, ",")) !== FALSE) {
+          	$title = $getData[0];
+          	$description = $getData[1];
+          	if($title != ''){
+          		alt_oer_csv_make_sessions($title, $description);
+          	}
+           }
+      
+           fclose($file);  
+     }   
+}
 
 
-		// load acf json
-		add_filter('acf/settings/load_json', 'oerxdomains_json_load_point');
+function alt_oer_csv_make_speakers($first, $last){
+	$title = $last . ', ' . $first;
+	$args = array(
+	  'post_title'    => wp_strip_all_tags($title),
+	  'post_type'     => 'speaker',
+	  'post_status'  => 'publish',	 
+	);
+	$post_id = wp_insert_post($args);
+	update_field('first_name', $first, $post_id);
+	update_field('last_name', $last, $post_id);
+}
 
-		function oerxdomains_json_load_point( $paths ) {
-		    
-		    // remove original path (optional)
-		    unset($paths[0]);
-		    
-		    
-		    // append path
-		    $paths[] = plugin_dir_path(__FILE__) . '/acf-json';//replace w get_stylesheet_directory() for theme
-		    
-		    
-		    // return
-		    return $paths;
-		    
-		}
+function alt_oer_parse_speaker_csv($file_path){
+	 if(isset($file_path)){
+    
+    	$filename = $file_path;    
+    
+        $file = fopen($filename, "r");
+        fgetcsv($file);
+          while (($getData = fgetcsv($file, 1000, ",")) !== FALSE) {
+          	$first = $getData[0];
+          	$last = $getData[1];
+          	if($first != '' && $last != ''){
+          		alt_oer_csv_make_speakers($first, $last);
+          	}
+           }
+      
+           fclose($file);  
+     }   
+}
+
+add_shortcode( 'mess', 'alt_oer_clean_mess' );
+
+
+function alt_oer_clean_mess(){
+	$args = array(
+			'post_type' => array('speaker'),
+			'title' => ',',
+	);
+
+
+	$the_query = new WP_Query( $args );
+
+	// The Loop
+	if ( $the_query->have_posts() ) :
+	while ( $the_query->have_posts() ) : $the_query->the_post();
+	  // Do Stuff
+		wp_trash_post(get_the_ID());
+	endwhile;
+	endif;
+
+	// Reset Post Data
+	wp_reset_postdata();		
+}
--- index.php
+++ index.php
@@ -51,3 +51,82 @@ add_filter('rest_prepare_presentation', 'acf_to_rest_api_presentation', 10, 3);
 //    return $data;
 // }, 10, 2 );
 
+//save acf json
+
+//deal with me making the import do  last comma first . . . 
+
+add_filter( 'the_title', 'oerxdomain_flip_speaker_name' );
+function oerxdomain_flip_speaker_name($title){
+   global $post;
+   $post_id = $post->ID;
+   $type = get_post_type($post_id);
+   if($type === 'speaker'){
+      return oerxdomain_flip_it($title);
+   } else {
+      return $title;
+   }
+}
+
+function oerxdomain_flip_it($title){
+   if(strpos($title, ',')>0){ //server is not running php 8 so str_contains is not an option
+      $pieces = explode(',', $title);
+      $first = $pieces[1];
+      $last = $pieces[0];
+      return $first . ' ' . $last;
+   } else {
+      return $title;
+   }
+   
+}
+
+add_filter('acf/settings/save_json', 'oerxdomains_json_save_point');
+ 
+function oerxdomains_json_save_point( $path ) {
+    
+    // update path
+    $path = plugin_dir_path(__FILE__) . '/acf-json'; //replace w get_stylesheet_directory() for theme
+    
+    
+    // return
+    return $path;
+    
+}
+
+
+// load acf json
+add_filter('acf/settings/load_json', 'oerxdomains_json_load_point');
+
+function oerxdomains_json_load_point( $paths ) {
+    
+    // remove original path (optional)
+    unset($paths[0]);
+    
+    
+    // append path
+    $paths[] = plugin_dir_path(__FILE__) . '/acf-json';//replace w get_stylesheet_directory() for theme
+    
+    
+    // return
+    return $paths;
+    
+}
+
+if ( ! function_exists('write_log')) {
+   function write_log ( $log )  {
+      if ( is_array( $log ) || is_object( $log ) ) {
+         error_log( print_r( $log, true ) );
+      } else {
+         error_log( $log );
+      }
+   }
+}
+
+function altoer_custom_mime_types( $mimes ) {
+
+        // New allowed mime types.
+       
+        $mimes['csv'] = 'text/csv';
+
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'altoer_custom_mime_types', 999,1 );
